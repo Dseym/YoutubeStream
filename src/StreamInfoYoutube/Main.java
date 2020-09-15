301b3dd6a6cc1f983ab04f5133d19db4bda57323
Dseym <kkovylin7@gmail.com>
2020-09-15 14:17:15 +0500

    StreamInfoYouTube BETA

---
+++ src/StreamInfoYoutube/Main.java
@@ -0,0 +1,39 @@
+package StreamInfoYoutube;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public static Main plugin;
+	
+	public void onEnable() {
+		
+		plugin = this;
+		
+		File config = new File(this.getDataFolder() + File.separator + "config.yml");
+		
+		if(!config.exists()) {
+			
+			try {
+				
+				config.createNewFile();
+				
+			} catch (IOException e) {}
+			
+		}
+		FileConfiguration configFile = (FileConfiguration)YamlConfiguration.loadConfiguration(config);
+		if(configFile.contains("api"))
+			YouTube.api = configFile.getString("api");
+		else
+			getLogger().warning("ERROR: Укажите API в конфиге");
+		
+		this.getLogger().info("Started!");
+		
+	}
+	
+}
